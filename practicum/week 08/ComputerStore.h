9ec33db476693ae6d96ab3d3a5e697acc1992628
Kristina2607 <krisi260705@gmail.com>
2025-05-01 13:17:11 +0300

    Update ComputerStore.h

--- practicum/week 08/ComputerStore.h
+++ practicum/week 08/ComputerStore.h
@@ -1,18 +1,27 @@
 #pragma once
 #include "Computer.h"
 
+enum class Criteria
+{
+	Gaming,
+	Travel,
+	NoCriteria
+};
+
 class ComputerStore
 {
 	Computer* computers;
 	size_t size;
 	size_t capacity;
 	char* name;
+
+	static constexpr size_t INITIAL_CAPACITY = 8;
+
 	void copyFrom(const ComputerStore& other);
 	void free();
 	void resize(size_t newCapacity);
 
 public:
-	ComputerStore();
 	ComputerStore(const char* name);
 	ComputerStore(const ComputerStore& other);
 	ComputerStore& operator= (const ComputerStore& other);
@@ -24,12 +33,13 @@ public:
 	void addComputer(const Computer& other);
 	void print() const;
 	void buyComputer(const char* brand, double money);
-	void printAvailableComputers();
+	void printAvailableComputers(Criteria criteria) const;
 };
 
 class ConsultantUtils
 {
-	bool isGoodForGaming(const Computer& obj);
-	bool isGoodForTravel(const Computer& obj);
+public:
+	static bool isGoodForGaming(const Computer& obj);
+	static bool isGoodForTravel(const Computer& obj);
 };
 
