48ac5d6932cde2cb30d23bdc61ac491e50e6c5c1
ivayloknchv <46057611+ivayloknchv@users.noreply.github.com>
2025-05-19 20:54:51 +0300

    added a missing virtual destr

--- Utils/K3 solution/ArrayTransformer.h
+++ Utils/K3 solution/ArrayTransformer.h
@@ -5,6 +5,6 @@ class ArrayTransformer
 public:
 	virtual void transform(int* arr, size_t size) const = 0;
 	virtual ArrayTransformer* clone() const = 0;
-	~ArrayTransformer() = default;
+	virtual ~ArrayTransformer() = default;
 };
 
